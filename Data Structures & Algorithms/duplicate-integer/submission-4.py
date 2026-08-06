cd093995894184410c6b06ef684771dbfcff94d3
brycejmaddox <brycejmaddox@gmail.com>
2026-08-05 22:57:07 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set(nums)
+        if len(nums) == len(seen):
+            return False
+        else:
+            return True
+            
