5bd39e4fb999279847f2f09ec80084a71e5a3c63
brycejmaddox <brycejmaddox@gmail.com>
2026-08-10 16:10:34 -0400

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left,right = 0, len(nums) -1
+        while left <= right:
+            m = (left + right) // 2
+            if nums[m] > target:
+                right = m - 1
+            elif nums[m] < target:
+                left = m + 1
+            else:
+                return m
+        return -1
+
+        
+        
