08e9baf964992124e7a6355487fd62de5958065f
brycejmaddox <brycejmaddox@gmail.com>
2026-08-12 22:12:05 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        flatten_matrix = [item for sublist in matrix for item in sublist]
+
+        l = 0
+        r = len(flatten_matrix) - 1
+
+        while l <= r:
+            if flatten_matrix[l] == target or flatten_matrix[r] == target:
+                return True
+            m = (l+r) // 2
+            if flatten_matrix[m] > target:
+                r = m - 1
+            elif flatten_matrix[m] < target:
+                l = m + 1
+            elif flatten_matrix[m] == target:
+                return True
+        return False
